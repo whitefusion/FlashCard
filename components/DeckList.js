d0e277651884c0837768604a5a4e5b518333511d
whitefusion <whitefusion@163.com>
2017-12-29 16:37:36 +0800

    add decklist component

---
+++ components/DeckList.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+import {Text, View, StyleSheet} from 'react-native'
+
+class DeckList extends Component {
+  render() {
+    return (
+      <Text> This is the main page </Text>
+    )
+  }
+} 
+
+export default DeckList
