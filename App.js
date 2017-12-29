19ca10373c433a77625552a60ac9801921d529a6
whitefusion <whitefusion@163.com>
2017-12-30 00:41:24 +0800

    Deploy redux

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import DeckList from './components/DeckList'
+import {createStore,applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import deck from './reducers'
+import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
 
 export default class App extends React.Component {
   render() {
+    const store = createStore(deck,composeWithDevTools(applyMiddleware(thunk)))
     return (
-      <View style={styles.container}>
-        <DeckList />
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <DeckList />
+        </View>
+      </Provider>
     );
   }
 }
