416d715b1f8cb607d5363771afd697ebe32fcfa8
whitefusion <whitefusion@163.com>
2017-12-30 23:36:34 +0800

    Add stylesheet

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,24 +1,75 @@
 import React,{Component} from 'react'
-import {Text, View, StyleSheet,FlatList} from 'react-native'
+import {Dimensions,Text, View, StyleSheet,FlatList} from 'react-native'
 import {connect} from 'react-redux'
 import {getDeck} from '../actions'
+import * as palette from '../utility/color'
+let {height, width} = Dimensions.get('window')
 
 class DeckList extends Component {
   componentDidMount(){
     this.props.getDeck()
   }
 
+  _keyExtractor = (item, index) => item.id;
+
   render() {
     console.log(this.props.deck)
     return (
       <View>
+        <View style={styles.headerContainer}>
+        <Text style={styles.header}> Deck List </Text>
+        </View>
+        <View style={styles.deckList}>
         <FlatList 
         data={Object.values(this.props.deck)}
-        renderItem={({item})=><Text>{JSON.stringify(item)}</Text>}
+        keyExtractor={this._keyExtractor}
+        renderItem={({item})=>(
+          <View style={styles.deckItem}>
+            <Text style={styles.deckTitle}>{item.title}</Text>
+            <Text style={styles.deckText}> {item.numCards} {item.numCards > 1 ? 'questions' : 'question'} </Text>
+          </View>
+        )}
         />
+        </View>
       </View>
     )
   }
 } 
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    height: 0.1*height,
+    borderBottomWidth: 2,
+    borderBottomColor: 'black',
+    marginBottom: 10,
+    alignItems: 'center',
+    justifyContent: "flex-end"
+  },
+  header:{
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  deckList : {
+    marginTop: 20,
+    alignItems: 'center'
+  },
+  deckItem : {
+    height: 0.1*height,
+    width: 0.8*width,
+    marginBottom:20,
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: palette.teal_blue,
+  },
+  deckTitle : {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: '800'
+  },
+  deckText: {
+    fontWeight: '600',
+    color: 'white'
+  }
+})
 
 export default connect((deck)=>({deck}),{getDeck})(DeckList)
