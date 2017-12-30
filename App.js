d7a112e5572f7ca2b9bf0503bf482a1acb6c05e2
whitefusion <whitefusion@163.com>
2017-12-30 23:37:43 +0800

    Add status bar component

--- App.js
+++ App.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import DeckList from './components/DeckList'
 import {createStore,applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
 import deck from './reducers'
 import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
+import {Constants} from 'expo'
+import NewDeck from './components/NewDeck'
+
+function HeadBar({...props}){
+  return (
+    <View style={{height:Constants.statusBarHeight}}>
+      <StatusBar {...props}/>
+    </View>
+  )
+}
 
 export default class App extends React.Component {
   render() {
     const store = createStore(deck,composeWithDevTools(applyMiddleware(thunk)))
     return (
       <Provider store={store}>
-        <View style={styles.container}>
+        <View style={{flex: 1}}>
+          {/* <HeadBar barStyle="default"/> */}
+          <NewDeck />
           <DeckList />
         </View>
       </Provider>
@@ -20,11 +32,4 @@ export default class App extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
