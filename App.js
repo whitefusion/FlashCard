36be04a22c781be6e1f15a0f0c3bcb01a722a70b
whitefusion <whitefusion@163.com>
2017-12-31 01:18:37 +0800

    Add navigation tab

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, 
+        StatusBar, Platform} from 'react-native';
 import DeckList from './components/DeckList'
 import {createStore,applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
@@ -8,6 +9,8 @@ import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import {Constants} from 'expo'
 import NewDeck from './components/NewDeck'
+import { TabNavigator } from 'react-navigation'
+import { MaterialIcons, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons'
 
 function HeadBar({...props}){
   return (
@@ -17,15 +20,33 @@ function HeadBar({...props}){
   )
 }
 
+const MainTabs = TabNavigator({
+  DeckList : {
+    screen: DeckList,
+    navigationOptions: {
+      tabBarLabel: "Decks",
+      tabBarIcon: <MaterialCommunityIcons name='cards-outline' size={25}/>
+    }
+  },
+  NewDeck: {
+    screen: NewDeck,
+    navigationOptions : {
+      tabBarLabel: "Add",
+      tabBarIcon: <MaterialIcons name='add-circle-outline' size={25}/>
+    }
+  }
+})
+
 export default class App extends React.Component {
   render() {
     const store = createStore(deck,composeWithDevTools(applyMiddleware(thunk)))
     return (
       <Provider store={store}>
         <View style={{flex: 1}}>
-          {/* <HeadBar barStyle="default"/> */}
-          <NewDeck />
-          <DeckList />
+          <HeadBar barStyle="default"/>
+          <MainTabs />
+          {/* <NewDeck /> */}
+          {/* <DeckList /> */}
         </View>
       </Provider>
     );
